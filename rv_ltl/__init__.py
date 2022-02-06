f4f688b65b0a655926a52de2faa43f645e671650
Shun <shunthedev@gmail.com>
2022-02-06 11:20:32 -0800

    export from `__init__.py`

--- rv_ltl/__init__.py
+++ rv_ltl/__init__.py
@@ -1,1 +1,18 @@
 __version__ = "0.1.0"
+
+from .b4 import B4
+from .core import (
+    MissingAtomicsException,
+    Node,
+    ConstantTrue,
+    ConstantFalse,
+    Atomic,
+    Not,
+    And,
+    Or,
+    Next,
+    Until,
+    Eventually,
+    Always,
+    Implies,
+)
