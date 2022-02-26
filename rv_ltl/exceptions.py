07b87ae81d96dcd75015cf73374dbb72334daaa4
Shun <shunthedev@gmail.com>
2022-02-26 12:54:05 -0800

    implement monitor

---
+++ rv_ltl/exceptions.py
@@ -0,0 +1,29 @@
+from typing import Set
+
+
+class MissingAtomicsException(Exception):
+    """
+    raised when `update` is called with state that does not contain all atomic
+    propositions on tree
+    """
+
+    def __init__(self, atomics: Set) -> None:
+        super().__init__(atomics)
+        self.atomics = atomics
+
+    def __str__(self) -> str:
+        names = []
+        for ap in self.atomics:
+            if ap.name != "":
+                names.append(ap.name)
+        unnamed_count = len(self.atomics) - len(names)
+        s = "Missing atomic propositions: "
+        s += ",".join(
+            names
+            + (
+                [f"{unnamed_count} unnamed atomic propotision(s)"]
+                if unnamed_count > 0
+                else []
+            )
+        )
+        return s
---
+++ rv_ltl/proposition.py
@@ -0,0 +1,117 @@
+from typing import Union
+from uuid import uuid4
+from .monitor import (
+    AlwaysMonitor,
+    AndMonitor,
+    EventuallyMonitor,
+    ImpliesMonitor,
+    Monitor,
+    AtomicMonitor,
+    NextMonitor,
+    NotMonitor,
+    OrMonitor,
+    UntilMonitor,
+)
+
+
+class Proposition:
+    def create_monitor(self) -> Monitor:
+        raise NotImplementedError()
+
+
+class Atomic(Proposition):
+    def __init__(self, *, identifier: Union[str, None] = None, name="") -> None:
+        """
+        Construct an atomic proposition.
+
+        Args:
+            identifier: Identifier that can be used in `update`
+            name: Name for the atomic proposition. Useful for debugging purposes.
+        """
+        super().__init__()
+        self.identifier = identifier if identifier is not None else str(uuid4())
+        self.name = name
+
+    def create_monitor(self) -> Monitor:
+        return AtomicMonitor(self)
+
+    def __str__(self) -> str:
+        # if name is specified, use it
+        if self.name != "":
+            return f"({self.name})"
+        # otherwise, use the identifier
+        return f"({self.identifier})"
+
+
+class Not(Proposition):
+    def __init__(self, op: Proposition) -> None:
+        super().__init__()
+        self.op = op
+
+    def create_monitor(self) -> Monitor:
+        return NotMonitor(self.op.create_monitor())
+
+
+class And(Proposition):
+    def __init__(self, *ops: Proposition) -> None:
+        super().__init__()
+        self.ops = ops
+
+    def create_monitor(self) -> Monitor:
+        return AndMonitor(*(op.create_monitor() for op in self.ops))
+
+
+class Or(Proposition):
+    def __init__(self, *ops: Proposition) -> None:
+        super().__init__()
+        self.ops = ops
+
+    def create_monitor(self) -> Monitor:
+        return OrMonitor(*(op.create_monitor() for op in self.ops))
+
+
+class Next(Proposition):
+    def __init__(self, op: Proposition) -> None:
+        super().__init__()
+        self.op = op
+
+    def create_monitor(self) -> Monitor:
+        return NextMonitor(self.op.create_monitor())
+
+
+class Until(Proposition):
+    def __init__(self, lhs: Proposition, rhs: Proposition) -> None:
+        super().__init__()
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def create_monitor(self) -> Monitor:
+        return UntilMonitor(self.lhs.create_monitor(), self.rhs.create_monitor())
+
+
+class Eventually(Proposition):
+    def __init__(self, op: Proposition) -> None:
+        super().__init__()
+        self.op = op
+
+    def create_monitor(self) -> Monitor:
+        return EventuallyMonitor(self.op.create_monitor())
+
+
+class Always(Proposition):
+    def __init__(self, op: Proposition) -> None:
+        super().__init__()
+        self.op = op
+
+    def create_monitor(self) -> Monitor:
+        return AlwaysMonitor(self.op.create_monitor())
+
+
+class Implies(Proposition):
+    def __init__(self, lhs: Proposition, rhs: Proposition) -> None:
+        super().__init__()
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def create_monitor(self) -> Monitor:
+        return ImpliesMonitor(self.lhs.create_monitor(), self.rhs.create_monitor())
